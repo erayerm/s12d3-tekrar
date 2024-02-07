d2c5d4bc2a01ad5f268134114202fc506fd25a10
erayerm <ermemiseray@gmail.com>
2024-02-07 12:39:05 +0300

    changes on Product Context

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,22 +1,9 @@
-import axios from "axios";
 import ProductCard from "./ProductCard";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { ProductContext } from "../context/ProductContext";
 
 export default function Products() {
-  //const [products, setProducts] = useState([]);
-  const { setProducts, products } = useContext(ProductContext);
-  const instance = axios.create({
-    baseURL: "https://fakestoreapi.com",
-    timeout: 1000,
-    headers: { Fakestoreapi: "foobar" },
-  });
-  useEffect(() => {
-    instance
-      .get("/products")
-      .then((res) => setProducts(res.data))
-      .catch((err) => console.error(err));
-  }, []);
+  const { products } = useContext(ProductContext);
 
   return (
     <div className="flex flex-wrap gap-5">
--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,15 +1,25 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const ProductContext = createContext();
 
 export const ProductContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-
+  const instance = axios.create({
+    baseURL: "https://fakestoreapi.com",
+    timeout: 1000,
+    headers: { Fakestoreapi: "foobar" },
+  });
+  useEffect(() => {
+    instance
+      .get("/products")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <ProductContext.Provider
       value={{
-        products,
-        setProducts,
+        products
       }}
     >
       {children}
