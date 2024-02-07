73c2d2a36f748ef090f730e7a9f5e08670dd662a
erayerm <ermemiseray@gmail.com>
2024-02-07 11:10:54 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+/*
+- Set up a new React application using Vite.
+- Integrate Axios for API calls to a public API (e.g., JSONPlaceholder or any open API of choice).
+- Display the fetched data in a component.
+- Implement basic state management with useState to manage the API response data.
+
+*/
+function App() {
+
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default App
