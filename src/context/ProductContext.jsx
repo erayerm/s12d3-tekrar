40a1bbbd814c875ad727ea1a144b2ddc327ce418
erayerm <ermemiseray@gmail.com>
2024-02-07 13:01:32 +0300

    useAxios hook

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,11 +1,14 @@
-import axios from "axios";
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
+import { useFetchData} from "../hooks/useAxios";
 
 export const ProductContext = createContext();
 
 export const ProductContextProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
-  const instance = axios.create({
+	const { data: products, loading, error } = useFetchData("/products");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  /*const instance = axios.create({
     baseURL: "https://fakestoreapi.com",
     timeout: 1000,
     headers: { Fakestoreapi: "foobar" },
@@ -16,6 +19,14 @@ export const ProductContextProvider = ({ children }) => {
       .then((res) => setProducts(res.data))
       .catch((err) => console.error(err));
   }, []);
+  */
+ if(error){
+    console.log(error);
+    return "";
+ }
+  if(loading){
+    return ""
+  }
   return (
     <ProductContext.Provider
       value={{
